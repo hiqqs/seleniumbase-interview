2dca2ded0e9723b59d2a862004fdfb316ba6988a
John Ahigian <jahigian@arkpes.com>
2021-06-01 13:46:00 -0400

    initial commit

---
+++ sbaseTest.py
@@ -0,0 +1,14 @@
+from seleniumbase import BaseCase
+
+class MyTestClass(BaseCase):
+
+    def test_basics(self):
+        self.open("https://www.saucedemo.com/")
+        
+    # standard user
+
+
+    # locked_out_user
+
+
+    # problem_user
